f965d55b7c4eecc1489bd32d0ff07b9f95c6b856
Sanjar0601 <105801077+Sanjar0601@users.noreply.github.com>
2026-01-19 02:51:43 +0500

    Finalize quiz submission and dashboard results

--- activity_builder/urls.py
+++ activity_builder/urls.py
@@ -8,7 +8,7 @@ from ninja import NinjaAPI
 
 from django.shortcuts import redirect
 from core.api import router as core_router
-from core.views import student_entry_view, quiz_entry_view, quiz_questions
+from core.views import student_entry_view, quiz_entry_view, quiz_questions, submit_quiz
 from core.auth_views import teacher_login, teacher_register, dashboard, public_library, add_assignment_to_list, add_group_to_list
 
 
@@ -41,6 +41,7 @@ urlpatterns = [
     path('test/<uuid:assignment_uuid>/', student_entry_view, name='student_entry'),
     path('quiz/<uuid:assignment_uuid>/entry/', quiz_entry_view, name='quiz_entry'),
     path('quiz/<uuid:assignment_uuid>/questions/', quiz_questions, name='quiz_questions'),
+    path('quiz/<uuid:assignment_uuid>/submit/', submit_quiz, name='submit_quiz'),
     
     # Root redirect
     path('', home_redirect, name='home'),
